4a8dc35b53f3b5c118322bc442b941b3dcaa9fcc
Seungwoo <angseung@vueron.org>
2024-05-16 12:07:13 +0900

    added files for preprocessing

--- includes/params.h
+++ includes/params.h
@@ -1,2 +1,6 @@
 #define MAX_POINTS_NUM 100000
 #define PCD_PATH "./pcd/*.pcd"
+
+// Params for Model
+#define NUM_POINT_VALUES 4
+#define INTENSITY_NORMALIZE_DIV 255
---
+++ includes/type.h
@@ -0,0 +1,24 @@
+// #include "params.h"
+// #include <iostream>
+
+// namespace vueron {
+// struct VoxelCoord {
+//     size_t _, __, y, x;
+// };
+
+// struct Point {
+//     float x, y, z;
+// #if NUM_POINT_VALUES >= 4
+//     float w;
+// #endif
+// };
+
+// struct Voxel {
+//     float x, y, z;
+// #if NUM_POINT_VALUES >= 4
+//     float w;
+// #endif
+//     float offset_from_mean_x, offset_from_mean_y, offset_from_mean_z;
+//     float offset_from_center_x, offset_from_center_y, offset_from_center_z;
+// };
+// } // namespace vueron
