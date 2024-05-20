eb5c99e4706ae4cc7d856d12410d342817c1676e
Seungwoo <angseung@vueron.org>
2024-05-20 13:20:43 +0900

    rm unused lines

--- src/pre.cpp
+++ src/pre.cpp
@@ -3,10 +3,3 @@
 #include "onnxruntime_cxx_api.h"
 #include <iostream>
 #include <vector>
-
-// 입력 데이터 크기
-const size_t BATCH_SIZE = 25000;
-const size_t FEATURE_SIZE1 = 20;
-const size_t FEATURE_SIZE2 = 10;
-
-namespace vueron {} // namespace vueron
