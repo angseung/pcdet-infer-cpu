19a65657641f8fffb1c105e1e0915c8ed3db7d8b
angseung <angseung@outlook.com>
2024-05-26 12:27:23 +0900

    added iou_rect

--- includes/post.h
+++ includes/post.h
@@ -12,6 +12,11 @@ namespace vueron {
 
 void rectify_score(std::vector<float> hm, std::vector<float> iou) {
     assert(hm.size() / CLASS_NUM == iou.size());
+    std::vector<float> rect_scores(IOU_RECTIFIER);
+    float alpha = rect_scores[l];
+    float iou = (*(_iou + i) + 1) * 0.5;
+    *(_score + i) =
+        powf(*(_score + i), (1 - alpha)) * powf(clip(iou, 0., 1.), alpha);
 }
 
 void decode_to_boxes(std::vector<std::vector<float>> &rpn_output) {
