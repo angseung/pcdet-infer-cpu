46083bdc1a17d03668485cab9f945420291b2dfb
angseung <angseung@outlook.com>
2024-05-28 22:29:06 +0900

    refactored post

--- includes/post.h
+++ includes/post.h
@@ -43,7 +43,8 @@ void decode_to_boxes(const std::vector<std::vector<float>> &rpn_output,
                       [&](size_t A, size_t B) { return hm[A] > hm[B]; });
 
     for (size_t j = 0; j < indices.size(); j++) {
-        size_t idx = indices[j];
+        size_t channel_offset = FEATURE_X_SIZE * FEATURE_Y_SIZE;
+        size_t idx = indices[j] % channel_offset;
 #ifdef _DEBUG
         std::cout << indices[j] << " : " << sigmoid(hm[indices[j]])
                   << std::endl;
@@ -51,35 +52,37 @@ void decode_to_boxes(const std::vector<std::vector<float>> &rpn_output,
         BndBox box;
         size_t grid_x = idx % FEATURE_X_SIZE;
         size_t grid_y = (idx / FEATURE_X_SIZE) % FEATURE_Y_SIZE;
-        size_t label = idx / (FEATURE_Y_SIZE * FEATURE_X_SIZE);
+        size_t label = idx / channel_offset;
         assert(grid_x < FEATURE_X_SIZE);
         assert(grid_y < FEATURE_Y_SIZE);
         assert(label >= 0 && label < FEATURE_NUM);
 
+        // calc box dimensions
         box.dx = exponential(rpn_output[1][idx]);
-        box.dy =
-            exponential(rpn_output[1][FEATURE_Y_SIZE * FEATURE_X_SIZE + idx]);
-        box.dz = exponential(
-            rpn_output[1][2 * FEATURE_Y_SIZE * FEATURE_X_SIZE + idx]);
+        box.dy = exponential(rpn_output[1][channel_offset + idx]);
+        box.dz = exponential(rpn_output[1][2 * channel_offset + idx]);
 
+        // calc heading angle in radian
         float cos_rad = rpn_output[4][idx];
-        float sin_rad = rpn_output[4][FEATURE_Y_SIZE * FEATURE_X_SIZE + idx];
+        float sin_rad = rpn_output[4][channel_offset + idx];
         box.heading = atan2(sin_rad, cos_rad);
         assert(box.heading <= 180.0 / M_PI && box.heading >= -180.0 / M_PI);
 
+        // calc center point
         box.x = head_stride * VOXEL_X_SIZE * (grid_x + rpn_output[2][idx]) +
                 MIN_X_RANGE;
         box.y = head_stride * VOXEL_Y_SIZE *
-                    (grid_y +
-                     rpn_output[2][FEATURE_Y_SIZE * FEATURE_X_SIZE + idx]) +
+                    (grid_y + rpn_output[2][channel_offset + idx]) +
                 MIN_Y_RANGE;
         box.z = rpn_output[3][idx];
 
-        float curr_iou = rpn_output[5][idx];
-
-        boxes[j] = box;
+        // append decodes boxes, scores and labels
+        float curr_iou =
+            rpn_output[5][idx]; // rectifying score if model has iou head
         scores[j] =
             rectify_score(sigmoid(hm[idx]), curr_iou, rect_scores[label]);
+
+        boxes[j] = box;
         labels[j] = label + 1;
         int a = 1;
     }
