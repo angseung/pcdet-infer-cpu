efd31f61139dc6ffb4ec3b39eab9ce79a353d0d6
Seungwoo <angseung@vueron.org>
2024-05-29 14:45:31 +0900

    fixed indices bug in post

--- includes/post.h
+++ includes/post.h
@@ -28,7 +28,7 @@ void decode_to_boxes(const std::vector<std::vector<float>> &rpn_output,
                      std::vector<BndBox> &boxes, std::vector<size_t> &labels,
                      std::vector<float> &scores) {
     /*
-    rpn_output order: hm, dim, center, center_z, rot, iou
+        rpn_output order: hm, dim, center, center_z, rot, iou
     */
     assert(rpn_output[0].size() ==
            CLASS_NUM * FEATURE_Y_SIZE * FEATURE_X_SIZE);                 // hm
@@ -46,20 +46,28 @@ void decode_to_boxes(const std::vector<std::vector<float>> &rpn_output,
     std::vector<size_t> indices(hm.size());
     std::vector<float> rect_scores(IOU_RECTIFIER);
     assert(rect_scores.size() == 3);
+
+    /*
+        get topk scores and their indices
+    */
     std::iota(indices.begin(), indices.end(), 0);
     std::partial_sort(indices.begin(), indices.begin() + MAX_BOX_NUM_BEFORE_NMS,
                       indices.end(),
                       [&](size_t A, size_t B) { return hm[A] > hm[B]; });
 
+    /*
+        decode into boxes
+    */
     for (size_t j = 0; j < MAX_BOX_NUM_BEFORE_NMS; j++) {
         size_t channel_offset = FEATURE_X_SIZE * FEATURE_Y_SIZE;
         size_t idx = indices[j];
-#ifdef _DEBUG
-        std::cout << indices[j] << " : " << sigmoid(hm[idx]) << std::endl;
-#endif
+        size_t s_idx = idx % (FEATURE_X_SIZE * FEATURE_Y_SIZE);
+        assert(idx < CLASS_NUM * FEATURE_X_SIZE * FEATURE_Y_SIZE);
+        assert(s_idx < FEATURE_X_SIZE * FEATURE_Y_SIZE);
+
+        // calc grid index
         BndBox box;
         size_t label = idx / channel_offset;
-        idx = idx % (FEATURE_X_SIZE * FEATURE_Y_SIZE);
         size_t grid_x = idx % FEATURE_X_SIZE;
         size_t grid_y = (idx / FEATURE_X_SIZE) % FEATURE_Y_SIZE;
         assert(grid_x < FEATURE_X_SIZE);
@@ -67,35 +75,33 @@ void decode_to_boxes(const std::vector<std::vector<float>> &rpn_output,
         assert(label >= 0 && label < FEATURE_NUM);
 
         // calc box dimensions
-        box.dx = exp(rpn_output[1][idx]);
-        box.dy = exp(rpn_output[1][channel_offset + idx]);
-        box.dz = exp(rpn_output[1][2 * channel_offset + idx]);
+        box.dx = exp(rpn_output[1][s_idx]);
+        box.dy = exp(rpn_output[1][channel_offset + s_idx]);
+        box.dz = exp(rpn_output[1][2 * channel_offset + s_idx]);
 
         // calc heading angle in radian
-        float cos_rad = rpn_output[4][idx];
-        float sin_rad = rpn_output[4][channel_offset + idx];
+        float cos_rad = rpn_output[4][s_idx];
+        float sin_rad = rpn_output[4][channel_offset + s_idx];
         box.heading = atan2(sin_rad, cos_rad);
-        assert(idx < FEATURE_X_SIZE * FEATURE_Y_SIZE);
         assert(box.heading <= 180.0 / M_PI && box.heading >= -180.0 / M_PI);
 
         // calc center point
-        box.x = head_stride * VOXEL_X_SIZE * (grid_x + rpn_output[2][idx]) +
+        box.x = head_stride * VOXEL_X_SIZE * (grid_x + rpn_output[2][s_idx]) +
                 MIN_X_RANGE;
         box.y = head_stride * VOXEL_Y_SIZE *
-                    (grid_y + rpn_output[2][channel_offset + idx]) +
+                    (grid_y + rpn_output[2][channel_offset + s_idx]) +
                 MIN_Y_RANGE;
-        box.z = rpn_output[3][idx];
+        box.z = rpn_output[3][s_idx];
 
         /*
-        append decoded boxes, scores, and labels
+            append decoded boxes, scores, and labels
         */
-        float curr_iou = rpn_output[5][idx];
+        float curr_iou = rpn_output[5][s_idx];
         // rectifying score if model has iou head
         scores[j] =
             rectify_score(sigmoid(hm[idx]), curr_iou, rect_scores[label]);
         boxes[j] = box;
         labels[j] = label + 1;
-        // int a = 1; // for break point
     }
 }
 
