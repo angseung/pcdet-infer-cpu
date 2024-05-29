6194efa646623f5e2d9618207057c07d7dc5d25a
Seungwoo <angseung@vueron.org>
2024-05-29 11:41:05 +0900

    test passed demo.cpp

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "npy.h"
 #include "params.h"
 #include "pcl.h"
 #include "post.h"
@@ -11,6 +12,8 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <vector>
+
+#define FROM_SNAPSHOT
 
 int main(int argc, const char **argv) {
     std::string folder_path;
@@ -26,10 +29,20 @@ int main(int argc, const char **argv) {
     size_t point_stride = NUM_POINT_VALUES;
 
     std::vector<std::string> pcd_files = vueron::getFileList(folder_path);
-    std::vector<float> buffer;
+#ifdef FROM_SNAPSHOT
+    std::string snapshot_folder_path = SNAPSHOT_PATH;
+    std::vector<std::string> snapshot_files =
+        vueron::getFileList(snapshot_folder_path);
+#endif
+    size_t num_test_files = pcd_files.size();
 
-    for (const auto &file : pcd_files) {
-        std::vector<float> buffer = vueron::readPcdFile(file, MAX_POINTS_NUM);
+    for (size_t i = 0; i < num_test_files; i++) {
+        std::string pcd_file = pcd_files[i];
+#ifdef FROM_SNAPSHOT
+        std::string snapshot_dir = snapshot_files[i];
+#endif
+        std::vector<float> buffer =
+            vueron::readPcdFile(pcd_file, MAX_POINTS_NUM);
         float *points_data = (float *)buffer.data();
         size_t points_buf_len = buffer.size();
 
@@ -67,6 +80,45 @@ int main(int argc, const char **argv) {
         vueron::rpn_run(bev_image, rpn_outputs);
         vueron::decode_to_boxes(rpn_outputs, boxes, labels, scores);
 
+#ifdef FROM_SNAPSHOT
+        /*
+            Read bev_features from snapshot file
+        */
+        // boxes
+        const std::string boxes_path = snapshot_dir + "/final_boxes.npy";
+        auto raw_boxes = npy::read_npy<float>(boxes_path);
+        std::vector<float> boxes_snapshot = raw_boxes.data;
+
+        // scores
+        const std::string scores_path = snapshot_dir + "/final_scores.npy";
+        auto raw_scores = npy::read_npy<float>(scores_path);
+        std::vector<float> scores_snapshot = raw_scores.data;
+
+        // labels
+        const std::string labels_path = snapshot_dir + "/final_labels.npy";
+        auto raw_labels = npy::read_npy<uint32_t>(labels_path);
+        std::vector<uint32_t> labels_snapshot = raw_labels.data;
+
+        std::vector<vueron::BndBox> s_boxes(scores_snapshot.size());
+        std::vector<float> s_scores(scores_snapshot.size());
+        std::vector<size_t> s_labels(labels_snapshot.size());
+
+        assert(boxes_snapshot.size() == 7 * labels_snapshot.size());
+        assert(scores_snapshot.size() == labels_snapshot.size());
+
+        for (size_t j = 0; j < labels_snapshot.size(); j++) {
+            s_scores[j] = scores_snapshot[j];
+            s_labels[j] = (size_t)labels_snapshot[j];
+            s_boxes[j].x = boxes_snapshot[7 * j];
+            s_boxes[j].y = boxes_snapshot[7 * j + 1];
+            s_boxes[j].z = boxes_snapshot[7 * j + 2];
+            s_boxes[j].dx = boxes_snapshot[7 * j + 3];
+            s_boxes[j].dy = boxes_snapshot[7 * j + 4];
+            s_boxes[j].dz = boxes_snapshot[7 * j + 5];
+            s_boxes[j].heading = boxes_snapshot[7 * j + 6];
+        }
+#endif
+
         /*
             Logging
         */
@@ -76,13 +128,18 @@ int main(int argc, const char **argv) {
                                      [](int i) { return i == 2; });
         auto cyc_cnt = std::count_if(labels.begin(), labels.end(),
                                      [](int i) { return i == 3; });
-        std::cout << "Input file: " << file << std::endl;
+        std::cout << "Input file: " << pcd_file << std::endl;
         std::cout << "vehicle(" << std::setw(3) << veh_cnt << "), pedestrian("
                   << std::setw(3) << ped_cnt << "), cyclist(" << std::setw(3)
                   << cyc_cnt << ")" << std::endl;
 
+#ifdef FROM_SNAPSHOT
+        auto image = drawBirdsEyeView(buffer.size() / point_stride, points_data,
+                                      s_boxes, s_scores, s_labels);
+#else
         auto image = drawBirdsEyeView(buffer.size() / point_stride, points_data,
                                       boxes, scores, labels);
+#endif
         cv::imshow("Bird's Eye View", image);
         cv::waitKey(1);
     }
