0553025e4d9f05fd3cb30c9c32c51e1c0d06d363
Seungwoo Im <angseung@outlook.com>
2024-06-08 17:55:27 +0900

    added const attrib in post.h

--- include/pcdet-infer-cpu/post.h
+++ include/pcdet-infer-cpu/post.h
@@ -42,10 +42,10 @@ inline float cross(const Point &p1, const Point &p2, const Point &p0) {
 
 inline int check_rect_cross(const Point &p1, const Point &p2, const Point &q1,
                             const Point &q2) {
-  int ret = min(p1.x, p2.x) <= max(q1.x, q2.x) &&
-            min(q1.x, q2.x) <= max(p1.x, p2.x) &&
-            min(p1.y, p2.y) <= max(q1.y, q2.y) &&
-            min(q1.y, q2.y) <= max(p1.y, p2.y);
+  const int ret = min(p1.x, p2.x) <= max(q1.x, q2.x) &&
+                  min(q1.x, q2.x) <= max(p1.x, p2.x) &&
+                  min(p1.y, p2.y) <= max(q1.y, q2.y) &&
+                  min(q1.y, q2.y) <= max(p1.y, p2.y);
   return ret;
 }
 
@@ -53,12 +53,14 @@ inline int check_in_box2d(const float *box, const Point &p) {
   // params: (7) [x, y, z, dx, dy, dz, heading]
   constexpr float MARGIN = 1e-2;
 
-  float center_x = box[0], center_y = box[1];
-  float angle_cos = cos(-box[6]),
-        angle_sin =
-            sin(-box[6]);  // rotate the point in the opposite direction of box
-  float rot_x = (p.x - center_x) * angle_cos + (p.y - center_y) * (-angle_sin);
-  float rot_y = (p.x - center_x) * angle_sin + (p.y - center_y) * angle_cos;
+  const float center_x = box[0], center_y = box[1];
+  const float angle_cos = cos(-box[6]);
+  const float angle_sin =
+      sin(-box[6]);  // rotate the point in the opposite direction of box
+  const float rot_x =
+      (p.x - center_x) * angle_cos + (p.y - center_y) * (-angle_sin);
+  const float rot_y =
+      (p.x - center_x) * angle_sin + (p.y - center_y) * angle_cos;
 
   return (fabs(rot_x) < box[3] / 2 + MARGIN &&
           fabs(rot_y) < box[4] / 2 + MARGIN);
@@ -66,9 +68,9 @@ inline int check_in_box2d(const float *box, const Point &p) {
 
 inline void rotate_around_center(const Point &center, const float angle_cos,
                                  const float angle_sin, Point &p) {
-  float new_x =
+  const float new_x =
       (p.x - center.x) * angle_cos + (p.y - center.y) * (-angle_sin) + center.x;
-  float new_y =
+  const float new_y =
       (p.x - center.x) * angle_sin + (p.y - center.y) * angle_cos + center.y;
   p.set(new_x, new_y);
 }
@@ -84,23 +86,25 @@ inline int intersection(const Point &p1, const Point &p0, const Point &q1,
   if (check_rect_cross(p0, p1, q0, q1) == 0) return 0;
 
   // check cross standing
-  float s1 = cross(q0, p1, p0);
-  float s2 = cross(p1, q1, p0);
-  float s3 = cross(p0, q1, q0);
-  float s4 = cross(q1, p1, q0);
+  const float s1 = cross(q0, p1, p0);
+  const float s2 = cross(p1, q1, p0);
+  const float s3 = cross(p0, q1, q0);
+  const float s4 = cross(q1, p1, q0);
 
   if (!(s1 * s2 > 0 && s3 * s4 > 0)) return 0;
 
   // calculate intersection of two lines
-  float s5 = cross(q1, p1, p0);
+  const float s5 = cross(q1, p1, p0);
   if (fabs(s5 - s1) > EPS) {
     ans.x = (s5 * q0.x - s1 * q1.x) / (s5 - s1);
     ans.y = (s5 * q0.y - s1 * q1.y) / (s5 - s1);
 
   } else {
-    float a0 = p0.y - p1.y, b0 = p1.x - p0.x, c0 = p0.x * p1.y - p1.x * p0.y;
-    float a1 = q0.y - q1.y, b1 = q1.x - q0.x, c1 = q0.x * q1.y - q1.x * q0.y;
-    float D = a0 * b1 - a1 * b0;
+    const float a0 = p0.y - p1.y, b0 = p1.x - p0.x,
+                c0 = p0.x * p1.y - p1.x * p0.y;
+    const float a1 = q0.y - q1.y, b1 = q1.x - q0.x,
+                c1 = q0.x * q1.y - q1.x * q0.y;
+    const float D = a0 * b1 - a1 * b0;
 
     ans.x = (b0 * c1 - b1 * c0) / D;
     ans.y = (a1 * c0 - a0 * c1) / D;
@@ -116,8 +120,8 @@ inline float sigmoid(const float x) { return 1.0f / (1.0f + exp(-x)); }
 
 inline float rectify_score(const float score, const float iou,
                            const float alpha) {
-  float new_iou = (iou + 1.0f) * 0.5f;
-  float new_score =
+  const float new_iou = (iou + 1.0f) * 0.5f;
+  const float new_score =
       powf(score, (1.0f - alpha)) * powf(clip(new_iou, 0.0f, 1.0f), alpha);
   return new_score;
 }
@@ -130,19 +134,16 @@ inline float box_overlap(const float *box_a, const float *box_b) {
   // params: box_a (7) [x, y, z, dx, dy, dz, heading]
   // params: box_b (7) [x, y, z, dx, dy, dz, heading]
 
-  //    float a_x1 = box_a[0], a_y1 = box_a[1], a_x2 = box_a[2], a_y2 =
-  //    box_a[3], a_angle = box_a[4]; float b_x1 = box_b[0], b_y1 = box_b[1],
-  //    b_x2 = box_b[2], b_y2 = box_b[3], b_angle = box_b[4];
-  float a_angle = box_a[6], b_angle = box_b[6];
-  float a_dx_half = box_a[3] / 2, b_dx_half = box_b[3] / 2,
-        a_dy_half = box_a[4] / 2, b_dy_half = box_b[4] / 2;
-  float a_x1 = box_a[0] - a_dx_half, a_y1 = box_a[1] - a_dy_half;
-  float a_x2 = box_a[0] + a_dx_half, a_y2 = box_a[1] + a_dy_half;
-  float b_x1 = box_b[0] - b_dx_half, b_y1 = box_b[1] - b_dy_half;
-  float b_x2 = box_b[0] + b_dx_half, b_y2 = box_b[1] + b_dy_half;
+  const float a_angle = box_a[6], b_angle = box_b[6];
+  const float a_dx_half = box_a[3] / 2, b_dx_half = box_b[3] / 2,
+              a_dy_half = box_a[4] / 2, b_dy_half = box_b[4] / 2;
+  const float a_x1 = box_a[0] - a_dx_half, a_y1 = box_a[1] - a_dy_half;
+  const float a_x2 = box_a[0] + a_dx_half, a_y2 = box_a[1] + a_dy_half;
+  const float b_x1 = box_b[0] - b_dx_half, b_y1 = box_b[1] - b_dy_half;
+  const float b_x2 = box_b[0] + b_dx_half, b_y2 = box_b[1] + b_dy_half;
 
-  Point center_a(box_a[0], box_a[1]);
-  Point center_b(box_b[0], box_b[1]);
+  const Point center_a(box_a[0], box_a[1]);
+  const Point center_b(box_b[0], box_b[1]);
 
   Point box_a_corners[5];
   box_a_corners[0].set(a_x1, a_y1);
@@ -157,8 +158,8 @@ inline float box_overlap(const float *box_a, const float *box_b) {
   box_b_corners[3].set(b_x1, b_y2);
 
   // get oriented corners
-  float a_angle_cos = cos(a_angle), a_angle_sin = sin(a_angle);
-  float b_angle_cos = cos(b_angle), b_angle_sin = sin(b_angle);
+  const float a_angle_cos = cos(a_angle), a_angle_sin = sin(a_angle);
+  const float b_angle_cos = cos(b_angle), b_angle_sin = sin(b_angle);
 
   for (int k = 0; k < 4; k++) {
     rotate_around_center(center_a, a_angle_cos, a_angle_sin, box_a_corners[k]);
@@ -232,14 +233,14 @@ inline float calculateIOU(const float *box_a, const float *box_b) {
   /*
       skip if distance of two boxes are large enough
   */
-  float dist = sqrt((box_a[0] - box_b[0]) * (box_a[0] - box_b[0]) +
-                    (box_a[1] - box_b[1]) * (box_a[1] - box_b[1]));
+  const float dist = sqrt((box_a[0] - box_b[0]) * (box_a[0] - box_b[0]) +
+                          (box_a[1] - box_b[1]) * (box_a[1] - box_b[1]));
   if (dist > PRE_NMS_DISTANCE_THD) {
     return 0.0f;
   }
-  float sa = box_a[3] * box_a[4];
-  float sb = box_b[3] * box_b[4];
-  float s_overlap = box_overlap(box_a, box_b);
+  const float sa = box_a[3] * box_a[4];
+  const float sb = box_b[3] * box_b[4];
+  const float s_overlap = box_overlap(box_a, box_b);
   return s_overlap / fmaxf(sa + sb - s_overlap, EPS);
 }
 
