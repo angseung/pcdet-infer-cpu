92d8881ab5aec63ff59bcad65773e0fe07e27f6c
Seungwoo Im <angseung@outlook.com>
2024-06-16 23:28:09 +0900

    separated main & demo

---
+++ include/demo.h
@@ -0,0 +1,23 @@
+#ifndef __DEMO_H__
+#define __DEMO_H__
+
+#include <glob.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+#include <cstdlib>
+#include <filesystem>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+#include "draw/draw.h"
+#include "npy.h"
+#include "pcdet-infer-cpu/common/metadata.h"
+#include "pcdet-infer-cpu/common/runtimeconfig.h"
+#include "pcdet-infer-cpu/pcdet.h"
+#include "pcl.h"
+
+namespace fs = std::filesystem;
+
+#endif //__DEMO_H__
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <filesystem>
+
+#include "npy.h"
+#include "pcdet-infer-cpu/common/metadata.h"
+#include "pcdet-infer-cpu/common/runtimeconfig.h"
+#include "pcdet-infer-cpu/pcdet.h"
+#include "pcl.h"
+#include "type.h"
+
+namespace fs = std::filesystem;
+
+#endif  //__MAIN_H__
