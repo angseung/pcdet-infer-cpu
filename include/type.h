85a48a2ee0c2e3a957110a2d0f852d39f64caaee
Seungwoo Im <angseung@outlook.com>
2024-07-14 16:08:25 +0900

    added type-cast operator for PredBox

--- include/type.h
+++ include/type.h
@@ -10,10 +10,17 @@ struct BndBox {
   explicit operator const float*() const {
     return &x;  // Return the address of the 'x' member
   }
+  explicit operator float() const = delete;
 };
 
 struct PredBox {
   float x, y, z, dx, dy, dz, heading, score, label;
+
+  // Operator overload to cast BndBox to const float*
+  explicit operator const float*() const {
+    return &x;  // Return the address of the 'x' member
+  }
+  explicit operator float() const = delete;
 };
 }  // namespace vueron
 
