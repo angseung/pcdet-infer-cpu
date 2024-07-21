dcff4144fa318c54ec402c38df026feaffb784ce
Seungwoo Im <angseung@outlook.com>
2024-07-21 18:50:25 +0900

    added comments on pcdet_c.cpp

--- src/pcdet-infer-cpu/pcdet_c.cpp
+++ src/pcdet-infer-cpu/pcdet_c.cpp
@@ -22,8 +22,12 @@ const char* get_pcdet_cpu_version(void) {
 void pcdet_initialize(const char* metadata_path,
                       const struct RuntimeConfig* runtimeconfig) {
   const std::string metadata_path_string{metadata_path};
+
+  // initialize model with metadata
   vueron::LoadMetadata(metadata_path_string);
   pcdet = std::make_unique<vueron::PCDetCPU>(PFE_FILE, RPN_FILE, runtimeconfig);
+
+  // logging configurations
   std::cout << vueron::GetMetadata() << std::endl;
   std::cout << *runtimeconfig << std::endl;
 }
@@ -31,18 +35,24 @@ void pcdet_initialize(const char* metadata_path,
 size_t pcdet_run(const float* points, const int point_buf_len,
                  const int point_stride, float** score, size_t** label,
                  Box** box) {
+  // check point input size
   assert(point_buf_len % point_stride == 0);
+
+  // run inference session
   pcdet->run(points, point_buf_len, point_stride, g_nms_pred, g_nms_labels,
              g_nms_score);
 
+  // check predicted buffer size
   assert(g_nms_pred.size() == g_nms_labels.size() &&
          g_nms_labels.size() == g_nms_score.size());
 
+  // copy address of output buffer to return pointers
   size_t num_preds = g_nms_labels.size();
   *score = g_nms_score.data();
   *label = g_nms_labels.data();
   *box = (Box*)g_nms_pred.data();
 
+  // clear global static buffers
   g_nms_score.clear();
   g_nms_pred.clear();
   g_nms_labels.clear();
@@ -51,7 +61,10 @@ size_t pcdet_run(const float* points, const int point_buf_len,
 }
 
 void pcdet_finalize(void) {
+  // destruct pcdet model
   pcdet = nullptr;
+
+  // clear global static buffers
   g_nms_score.clear();
   g_nms_pred.clear();
   g_nms_labels.clear();
