3445e3ed452b33e50e2f53d2a87b63bdca9ee103
Seungwoo Im <angseung@outlook.com>
2024-08-03 16:01:12 +0900

    TODO: draw bounding box on demo_3d.cpp

--- src/draw/draw.cpp
+++ src/draw/draw.cpp
@@ -84,13 +84,50 @@ void draw3DEyeView(const size_t point_buf_len, const size_t point_stride,
                                     points_data[point_stride * i + 1],
                                     points_data[point_stride * i + 2]);
   }
+
+  const std::vector<Eigen::Vector3d> colors(points_size,
+                                            Eigen::Vector3d(1.0, 1.0, 1.0));
+  pointcloud.colors_ = colors;
   open3d::visualization::Visualizer visualizer;
-  std::shared_ptr<open3d::geometry::PointCloud> pointcloud_ptr(
+  const std::shared_ptr<open3d::geometry::PointCloud> pointcloud_ptr(
       new open3d::geometry::PointCloud);
   *pointcloud_ptr = pointcloud;
-
   visualizer.CreateVisualizerWindow("Open3D", 1600, 900);
-  visualizer.AddGeometry(pointcloud_ptr);
+  // visualizer.AddGeometry(pointcloud_ptr);
+
+  for (size_t i = 0; i < scores.size(); i++) {
+    // Define the center of the cuboid
+    const BndBox box{boxes[i]};
+    Eigen::Vector3d center(box.x, box.y, box.z);
+
+    // Define the 3D dimensions of the cuboid
+    Eigen::Vector3d dimensions(box.dx, box.dy, box.dz);
+
+    // Define the rotation angles in radians (yaw, pitch, roll)
+    double yaw = M_PI / 4;    // 45 degrees
+    double pitch = M_PI / 6;  // 30 degrees
+    double roll = M_PI / 3;   // 60 degrees
+
+    // Create a rotation matrix from the Euler angles
+    Eigen::Matrix3d rotation =
+        (Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ()) *
+         Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()) *
+         Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX()))
+            .matrix();
+
+    // Create an oriented bounding box
+    open3d::geometry::OrientedBoundingBox obb(center, rotation, dimensions);
+
+    // Convert the oriented bounding box to a LineSet for visualization
+    auto obb_lines =
+        open3d::geometry::LineSet::CreateFromOrientedBoundingBox(obb);
+
+    // Add the oriented bounding box to the visualizer
+    visualizer.AddGeometry(obb_lines);
+  }
+
+  visualizer.GetRenderOption().background_color_ =
+      Eigen::Vector3d(0.0, 0.0, 0.0);
   visualizer.Run();
   visualizer.DestroyVisualizerWindow();
 }
