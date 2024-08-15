8e0d3e86dfecba48f4d32c0515d7a88a718205c0
Seungwoo Im <angseung@outlook.com>
2024-08-15 11:28:56 +0900

    Updated README.md

--- src/demo_c.cpp
+++ src/demo_c.cpp
@@ -87,7 +87,7 @@ int main(int argc, const char **argv) {
     */
     for (size_t box_index = 0; box_index < n_boxes; box_index++) {
       BndBox pred{preds[box_index]};
-      Box box{};
+      Box box{0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
       nms_labels.push_back(static_cast<size_t>(pred.label));
       nms_scores.push_back(pred.score);
       box.x = pred.x;
